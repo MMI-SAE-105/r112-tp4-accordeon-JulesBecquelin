1a9a5d1c04d77e483d66ac3278911c09b751f986
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2024-11-27 11:36:34 +0100

    ajout javascript + correction css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-// *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+const details = accordeon.querySelectorAll("details");
+
+
+details.forEach(det => det.open = false);
+
+
+details.forEach(detail => {
+  detail.addEventListener('toggle', () => {
+    if (detail.open) {
+     
+      details.forEach(otherDetail => {
+        if (otherDetail !== detail && otherDetail.open) {
+          otherDetail.open = false;
+        }
+      });
+    }
+  });
+});
